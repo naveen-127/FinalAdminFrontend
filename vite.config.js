822caac1df70148f96ee1b5dc58f3d9d4cf936b5
dinesh-2207 <226466647+dinesh-2207@users.noreply.github.com>
2026-06-05 16:03:07 +0530

    last commit

--- vite.config.js
+++ vite.config.js
@@ -11,6 +11,21 @@ export default {
         target: 'http://127.0.0.1:5000',
         changeOrigin: true,
         secure: false,
+      },
+      '/admin/': {
+        target: 'http://127.0.0.1:5000',
+        changeOrigin: true,
+        secure: false,
+      },
+      '/ai/': {
+        target: 'http://127.0.0.1:5000',
+        changeOrigin: true,
+        secure: false,
+      },
+      '/static/': {
+        target: 'http://127.0.0.1:5000',
+        changeOrigin: true,
+        secure: false,
       }
     }
   }
