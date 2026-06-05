3ca3b2e09ecbed02c619ca08cf07bba27b135866
dinesh-2207 <226466647+dinesh-2207@users.noreply.github.com>
2026-06-05 14:27:43 +0530

    last changes

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,13 @@ export default {
   server: {
     host: '0.0.0.0', // or true to use the system's IP
     port: 5173,
+    proxy: {
+      '/adminhome/derivation': {
+        target: 'http://127.0.0.1:5000',
+        changeOrigin: true,
+        secure: false,
+      }
+    }
   }
 }
 
